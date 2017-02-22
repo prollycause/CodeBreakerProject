56ae018d1739fc18b651bd31e055a48a0383cdc1
prollycause <prollycause@users.noreply.github.com>
2017-02-22 16:00:18 -0500

    changes.

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,27 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if (answer.value == '') || (attempt.value == '') {
+    	setHiddenFields();
+    }
+}
+//implement new functions here
+function setHiddenFields() {
+	answer.value = Math.floor(Math.random() * 10000).toString();
+	while(answer.value.length < 4) {
+		answer.value = "0" + answer.value;
+	}
+	attempt.value = "0";
 }
 
-//implement new functions here
+function setMessage(message) {
+	document.getElementById('message').innerHTML = message;
+}
+
+function validateInput(input) {
+	if ( input.length != 4 ) {
+		setMessage('Guesses must be exactly 4 characters long.');
+		return false;
+	}
+	return true;
+}
